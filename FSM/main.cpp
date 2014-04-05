8c9fc934782786eeaf231b01d92c753f1855e7ce
Kristof Niederholtmeyer <kristof.niederholtmeyer@rwth-aachen.de>
2014-04-05 19:05:41 +0200

    Initial Commit

---
+++ FSM/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  FSM
+//
+//  Created by Kristof Niederholtmeyer on 05.04.14.
+//  Copyright (c) 2014 Kristof Niederholtmeyer. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
